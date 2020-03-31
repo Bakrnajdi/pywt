698004b668fc9199a5a315bb97aefe47291a107b
Bakrnajdi <53707295+Bakrnajdi@users.noreply.github.com>
2020-03-31 19:24:37 +0100

    Update cwt.template.c

--- pywt/_extensions/c/cwt.template.c
+++ pywt/_extensions/c/cwt.template.c
@@ -226,9 +226,8 @@ void CAT(TYPE, _ben)(const TYPE * const restrict input, TYPE * const restrict ou
     size_t i = 0;
     for (i = 0; i < N; i++)
     {
-        output_r[i] = (1-CAT(TYPE, _pow)(input[i], 2.0))*CAT(TYPE, _exp)(-CAT(TYPE, _pow)(input[i], 2.0)/2)*2/(CAT(TYPE, _sqrt)(3)*CAT(TYPE, _sqrt)(CAT(TYPE, _sqrt)(-CAT(TYPE, _pi)())))*CAT(TYPE,_exp)(CAT(TYPE, _cos)(2*FC*input[i]));
-        output_i[i] = (1-CAT(TYPE, _pow)(input[i], 2.0))*CAT(TYPE, _exp)(-CAT(TYPE, _pow)(input[i], 2.0)/2)*2/(CAT(TYPE, _sqrt)(3)*CAT(TYPE, _sqrt)(CAT(TYPE, _sqrt)(-CAT(TYPE, _pi)())))*CAT(TYPE,_exp)(CAT(TYPE, _sin)(2*FC*input[i]));
-
+        output[i] = (1-CAT(TYPE, _pow)(input[i], 2.0))*CAT(TYPE, _exp)(-CAT(TYPE, _pow)(input[i], 2.0)/2)*2/(CAT(TYPE, _sqrt)(3)*CAT(TYPE, _sqrt)(CAT(TYPE, _sqrt)(CAT(TYPE, _pi)())));
+        
     }
 }
 
